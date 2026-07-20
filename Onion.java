aeb0fc870d4a0388ddc7fe2a463337e6a4619660
stanng66 <stan.ng66@gmail.com>
2026-07-20 12:53:27 -0400

    Set up file and comments in Onion.java
    Updated comments to reflect assignment requirements for Onion class.

---
+++ Onion.java
@@ -0,0 +1,17 @@
+// ====================
+// Assignment 4
+// Stanley Nguyen
+// Humber College
+// CPAN-131-RNA
+// Mehrnaz Zhian
+// July 28, 2026
+// --------------------
+// This program demonstrates 
+// ====================
+// Assignment Requirement: Create 2nd Child :This will have private int attribute as layers 
+
+// Assignment Requirement: Onion inheriting from vegetable:
+
+// Assignment Requirement: Create 2 constructors: pass color and name in super and set the layers with current attribute 
+
+// Assignment Requirement: Implement the body of isTasty.If you like the vegetable then return true
