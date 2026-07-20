eba469cb2d0b764fcb941ee700cacba4d09785be
stanng66 <stan.ng66@gmail.com>
2026-07-20 12:50:48 -0400

    Set up file and comments in Carrot.java
    Added comments for class structure and implementation details.

---
+++ Carrot.java
@@ -0,0 +1,17 @@
+// ====================
+// Assignment 4
+// Stanley Nguyen
+// Humber College
+// CPAN-131-RNA
+// Mehrnaz Zhian
+// July 28, 2026
+// --------------------
+// This program demonstrates 
+// ====================
+// Create 1st Child
+
+// Carrot inheriting from vegetable
+
+// Create 2 constructor ,pass color and name in super 
+
+// Implement the body of isTasty.If you like the vegetable then return true
