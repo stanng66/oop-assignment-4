de8a8b3c096902b5e0e529ea43a0f0682cce41bb
stanng66 <stan.ng66@gmail.com>
2026-07-20 12:56:17 -0400

    Set up file and comments in TestVegetable.java

---
+++ TestVegetable.java
@@ -0,0 +1,21 @@
+// ====================
+// Assignment 4
+// Stanley Nguyen
+// Humber College
+// CPAN-131-RNA
+// Mehrnaz Zhian
+// July 28, 2026
+// --------------------
+// This program demonstrates 
+// ====================
+// Create TestVegetable Class : This will have main method
+
+// Polymorphism:1) Create Vegetable v1 of type carrot and 2) Vegetable v2 of type onion with 10 layers.
+
+//  for each object
+
+// If the isTasty is true then print I like stirfry of this vegetable
+
+// If the isTasty is false then print I don't like stirfry of this vegetable
+
+// Now create an example opf downcasting using instanceof variable and show that v2 is carrot and v2 is onion
