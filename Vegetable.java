621535a1890a945ebad4c4f5037bfa0f83b0c3f6
stanng66 <stan.ng66@gmail.com>
2026-07-20 12:48:16 -0400

    Set up file and comments for Vegetable.java
    Updated comments and added assignment requirements for the Vegetable class.

---
+++ Vegetable.java
@@ -0,0 +1,19 @@
+// ====================
+// Assignment 4
+// Stanley Nguyen
+// Humber College
+// CPAN-131-RNA
+// Mehrnaz Zhian
+// July 28, 2026
+// --------------------
+// This program demonstrates 
+// ====================
+// Assignment Requirement: Create abstract class called Vegetable. 
+
+// Assignment Requirement: This class will have 2 Private String: attributes color and name
+
+// Assignment Requirement: Create default and 2 args constructors
+
+// Assignment Requirement: create getter and setter
+
+// Assignment Requirement: This class will have abstract method as isTasty The return type for this method is boolean
